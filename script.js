ff0dadc38f45c6c05da979a1bf9e6629bebe48c4
YagoMartins5 <>
2022-04-19 16:55:57 +0000

    crescimento da cobrinha ao comer ao comida

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ snake[0] = {
   y: 8 * box
 }
 let direction = "right";
+let food = {
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box
+}
+
 function criarBG() {
   context.fillStyle = 'lightgreen';
   context.fillRect(0, 0, 16 * box, 16 * box);
@@ -17,6 +22,11 @@ function criarCobrinha() {
   context.fillStyle = "green";
   context.fillRect(snake[i].x, snake[i].y, box, box);
   }
+}
+
+function drawFood(){
+  context.fillStyle = "red";
+  context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -37,6 +47,7 @@ function iniciarJogo(){
   
   criarBG();
   criarCobrinha();
+  drawFood();
   
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
@@ -45,8 +56,14 @@ function iniciarJogo(){
   if (direction == "left") snakeX -= box;
   if (direction == "up") snakeY -= box;
   if (direction == "down") snakeY += box;
-  
+
+if (snakeX != food.x || snakeY != food.y){
   snake.pop();
+} else {
+  food.x: Math.floor(Math.random() * 15 + 1) * box,
+  food.y: Math.floor(Math.random() * 15 + 1) * box
+}
+
   
   let newHead = {
     x: snakeX,
