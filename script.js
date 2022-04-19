033cb40309375932590148526a57452144b04e80
yago <yagowill3@gmail.com>
2022-04-19 20:02:49 -0300

    mudança de cores

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ let food = {
 }
 
 function criarBG() {
-  context.fillStyle = 'lightgreen';
+  context.fillStyle = 'lightblue';
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha() {
   for (i = 0 ; i < snake.length ; i++) {
-  context.fillStyle = "green";
+  context.fillStyle = "darkgreen";
   context.fillRect(snake[i].x, snake[i].y, box, box);
   }
 }
